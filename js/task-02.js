e25247831cd45d8abcae4c8cd58d7ff7210e9b51
ooolmaaak <131387966+ooolmaaak@users.noreply.github.com>
2024-01-25 19:44:15 +0200

    hw-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,30 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("ul");
+console.log("list:", list);
+
+// нужно сделать ф-цию которая будет добавлять в список новый li с именем, вытянутым
+// из ingredients, ingredients нужно перебрать
+
+const ingredientList = (name) => {
+  const newListEl = document.createElement("li");
+  newListEl.classList.add("item");
+
+  const listName = document.createElement("default");
+  listName.textContent = name;
+
+  newListEl.append(listName);
+  return newListEl;
+};
+
+const ingredientsNames = ingredients.map(ingredientList);
+console.log(ingredientsNames);
+
+list.append(...ingredientsNames);

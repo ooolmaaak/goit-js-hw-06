d36305c99cbdaff70e869b89b2aa844b0bb56c5f
ooolmaaak <131387966+ooolmaaak@users.noreply.github.com>
2024-01-26 17:29:45 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,22 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+// получаем доступ к тексту, в котором будет указан цвет
+const colorSpan = document.querySelector(".color");
+
+// получаем доступ к нопке, меняющей цвет фона по клику
+const changeColorButton = document.querySelector(".change-color");
+
+// вешаем слушателя события на кнопку, событие "клик"
+changeColorButton.addEventListener("click", () => {
+  // создаем новую переменную в которой запустим функцию генерации и
+  // будем хранить в ней новое сгенерированное число
+  const newColor = getRandomHexColor();
+
+  //получаем доступ к body => style of body => background color of body
+  document.body.style.backgroundColor = newColor;
+
+  // устанавливаем значение текста, которое равно сгенерированному цвету
+  colorSpan.textContent = newColor;
+});

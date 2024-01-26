357b416776245b122f27a006d5dc925c440f4684
ooolmaaak <131387966+ooolmaaak@users.noreply.github.com>
2024-01-26 02:38:00 +0200

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,20 @@
+// Написать скрипт который, при наборе текста в инпуте input#name-input
+// (событие input), подставляет его текущее значение в span#name - output.
+// Если инпут пустой, в спане должна отображаться строка "Anonymous".
+
+const textInput = document.getElementById("name-input");
+const textOutput = document.getElementById("name-output");
+const defaultOutput = document.getElementById("name-output").textContent;
+// сохранили дефолт значение
+
+//пишем функцию для слушателя события
+textInput.addEventListener("input", (event) => {
+  const inputValue = event.currentTarget.value;
+
+  // проверяем, пустая строка или нет
+  if (inputValue !== "") {
+    textOutput.innerText = inputValue;
+  } else {
+    textOutput.innerText = defaultOutput;
+  }
+});
